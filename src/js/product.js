296b94ba32aaff62d1daf20c0c11b09884af373e
yamsir <yamsirgg@gmail.com>
2025-07-07 16:24:30 -0600

    Format add to cart button event listener for improved readability

--- src/js/product.js
+++ src/js/product.js
@@ -16,4 +16,6 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
-document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
